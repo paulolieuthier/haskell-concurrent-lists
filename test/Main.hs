3a20d75d0b25f46cb32e07b7ae50e3a3c3e6fca1
Paulo Lieuthier <paulolieuthier@gmail.com>
2017-07-05 23:51:05 -0300

    Add tests for simple removals

--- test/Main.hs
+++ test/Main.hs
@@ -9,6 +9,7 @@ main = hspec $ do
     forM_ L.allTypes $ \listType -> do
         describe (show listType ++ " implementation") $ do
             testInsertions listType
+            testRemovals listType
 
 testInsertions listType = do
     it "should insert in empty list" $ do
@@ -29,10 +30,48 @@ testInsertions listType = do
         list <- L.toPureList safeList
         list `shouldBe` [40, 42]
 
-    it "should fail to insert already existing value" $ do
+    it "should not insert already existing value" $ do
         safeList <- L.fromList listType [42]
         result <- L.add safeList 42
         result `shouldBe` False
 
         list <- L.toPureList safeList
         list `shouldBe` [42]
+
+testRemovals listType = do
+    it "should not remove missing element on list" $ do
+        forM_ [[], [41], [43], [41, 43]] $ \pureList -> do
+            safeList <- L.fromList listType pureList
+
+            result <- L.remove safeList 42
+            result `shouldBe` False
+
+            list <- L.toPureList safeList
+            list `shouldBe` (filter (/= 42) pureList)
+
+    it "should remove first element of list" $ do
+        safeList <- L.fromList listType [1, 2, 3]
+
+        result <- L.remove safeList 1
+        result `shouldBe` True
+
+        list <- L.toPureList safeList
+        list `shouldBe` [2, 3]
+
+    it "should remove last element of list" $ do
+        safeList <- L.fromList listType [1, 2, 3]
+
+        result <- L.remove safeList 3
+        result `shouldBe` True
+
+        list <- L.toPureList safeList
+        list `shouldBe` [1, 2]
+
+    it "should remove remove element from middle of list" $ do
+        safeList <- L.fromList listType [1, 2, 3]
+
+        result <- L.remove safeList 2
+        result `shouldBe` True
+
+        list <- L.toPureList safeList
+        list `shouldBe` [1, 3]
